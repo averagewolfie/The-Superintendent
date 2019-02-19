7b7384e6889aa8236d2033a0333bf9a78b44eeff
Toron Audura Beldevar <the.average.wolfie@gmail.com>
2019-02-18 23:07:40 -0600

    mine
    An abbreviation for "minor improvements, nothing exciting".

--- extensions/new.py
+++ extensions/new.py
@@ -5,15 +5,14 @@ class New:
 	def __init__(self, bot):
 		self.bot = bot
 
-	@commands.command()
-	async def prune(self, ctx, amount:int):
+	@commands.command(aliases=["purge","remove","delete"])
+	async def prune(self, ctx, amount):
 		try:
-			if not type(amount) is int:
-				return await ctx.channel.send("You need to specify an *integer* value of messages to prune.")
-			await ctx.channel.purge(limit=amount + 1, bulk=True)
-			return await ctx.channel.send(content=str(amount) + " messages deleted! One additional removal has also occurred, for the message which you sent to invoke the command.")
-		except Exception as e:
-			print("An error has occurred during \"on_message()\".\n" + type(e).__name__ + ": " + str(e))
+			amount = int(amount)
+		except:
+			return await ctx.channel.send("You need to specify an *integer* value of messages to prune.")
+		await ctx.channel.purge(limit=amount + 1, bulk=True)
+		return await ctx.channel.send(content=str(amount) + " messages deleted! One additional removal has also occurred, for the message which you sent to invoke the command.")
 
 def setup(bot):
 	bot.add_cog(New(bot))
